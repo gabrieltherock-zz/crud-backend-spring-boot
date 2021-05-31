55eb9d9e719621fac3ba49b69c1f62710c3729b8
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-05-30 21:12:01 -0300

    Initial commit

---
+++ src/main/java/com/gabriel/crudlabengenhariabackend/CrudLabengenhariaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.crudlabengenhariabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudLabengenhariaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudLabengenhariaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabriel/crudlabengenhariabackend/CrudLabengenhariaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.crudlabengenhariabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudLabengenhariaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
