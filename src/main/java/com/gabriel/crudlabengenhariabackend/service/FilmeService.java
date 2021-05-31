f44a84679eda732b2b5d98a5fa6cd78ad9ef152d
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-05-30 23:56:52 -0300

    removendo modelMapper da classe de serviço

--- src/main/java/com/gabriel/crudlabengenhariabackend/service/FilmeService.java
+++ src/main/java/com/gabriel/crudlabengenhariabackend/service/FilmeService.java
@@ -2,7 +2,6 @@ package com.gabriel.crudlabengenhariabackend.service;
 
 import com.gabriel.crudlabengenhariabackend.model.Filme;
 import com.gabriel.crudlabengenhariabackend.repository.FilmeRepository;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +12,6 @@ public class FilmeService {
 
     @Autowired
     FilmeRepository filmeRepository;
-
-    @Autowired
-    ModelMapper modelMapper;
 
     public List<Filme> findAll() {
         return filmeRepository.findAll();
