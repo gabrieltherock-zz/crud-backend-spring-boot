a9f952c367ddfd4ad9ad27766696074d65dfb472
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-06-10 21:10:25 -0300

    colocando o @CrossOrigin nessa ************

--- src/main/java/com/gabriel/crudlabengenhariabackend/controller/FilmeController.java
+++ src/main/java/com/gabriel/crudlabengenhariabackend/controller/FilmeController.java
@@ -5,6 +5,7 @@ import com.gabriel.crudlabengenhariabackend.service.FilmeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000/", maxAge = 3600)
 @RestController
 public class FilmeController {
 
